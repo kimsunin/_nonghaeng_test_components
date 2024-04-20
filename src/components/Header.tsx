5d1dad8f4ed3448f8309a60b1f0e59464a3a39ec
김선인 <jumto10@naver.com>
2024-04-20 12:38:32 +0900

    test commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import { headerLinks } from "@/storage/linkData/linkData";
 
 function Header() {
+  const isLogin = true;
   const subMenuLinks = (subMenu) => (
-    <ul>
+    <ul className="w-full float-none">
       {subMenu.map((item, subIndex) => (
-        <li key={subIndex}>
+        <li key={subIndex} className=" float-none text-center w-full">
           <Link href={item.href}>{item.title}</Link>
         </li>
       ))}
@@ -14,9 +15,9 @@ function Header() {
   );
 
   const mainMenuLinks = headerLinks.map((menuItem, index) => (
-    <li key={index} className="relative">
+    <li key={index} className="relative w-auto text-center mx-4 inline-block">
       <Link href={menuItem.href}>{menuItem.title}</Link>
-      <div className="hidden absolute top-full group-hover:block bg-green-50">
+      <div className="hidden absolute mt-8 group-hover:block w-full bg-green-50">
         {menuItem.subMenu && subMenuLinks(menuItem.subMenu)}
       </div>
     </li>
@@ -24,22 +25,26 @@ function Header() {
 
   return (
     <>
-      <div className="fixed top-0 left-0 right-0 h-20 bg-white ">
+      <div className="fixed top-0 left-0 right-0 h-20 bg-green-100">
+        {/* Header높이 = 20 */}
         <h1>header</h1>
         <div className="flex">
-          <div>
+          <div className="fixed float-left">
             <Link href="/">홈</Link>
           </div>
-          <nav className="relative group">
-            <ul className="flex cursor-pointer">{mainMenuLinks}</ul>
+          <nav className="relative group hidden lg:block mx-0 w-full">
+            <ul className="flex cursor-pointer mx-20 w-full float-left">
+              {mainMenuLinks}
+            </ul>
           </nav>
-          <div>
+          <div className="fixed right-40 hidden lg:block float-right">
             <form>
               <input type="text" placeholder="정보를 찾고계신가요?"></input>
               <button>검색</button>
             </form>
           </div>
-          <div>
+          <div className="fixed right-4 ">
+            {isLogin && <Link href="/mypage/login">로그인</Link>}
             <Link href="/mypage">마이페이지</Link>
             <Link href="/sitemap">메뉴</Link>
           </div>
